9f2c19337dd4076b8b0a5a6ce40045928e4f567e
Rob Whitnell <rwhitnel@guilford.edu>
2021-05-04 16:32:48 -0400

    Final push of json testing

--- dyes/jsontest.js
+++ dyes/jsontest.js
@@ -21,4 +21,31 @@ function()  {
     // make a variable to hold the info
     myDyes = request.response;
     console.log(myDyes);
+    // call a function to add DOM elements based on the JSON content
+    makeMain(myDyes);
+}
+
+function makeMain(jsonObj) {
+    theDyes = jsonObj["dyes"];  // Provide the dyes list
+
+    for (i=0; i < theDyes.length; i++) { // Use each dye in the list, one after the other, and build a new DOM div from it
+        dyeDiv = document.createElement('div');
+        dyenameH1 = document.createElement('h1');
+        dyemanuH2 = document.createElement('h2');
+        dyeImage = document.createElement('img');
+
+        dyenameH1.textContent = theDyes[i].name;
+        dyemanuH2.textContent = theDyes[i].manufacturer;
+        dyeImage.src = theDyes[i].bottleimage;
+        dyeImage.alt = theDyes[i].alt;
+
+        dyenameH1.classList.add("chemicalname");
+
+        dyeDiv.appendChild(dyenameH1);
+        dyeDiv.appendChild(dyemanuH2);
+        dyeDiv.appendChild(dyeImage);
+
+        mainStuff.appendChild(dyeDiv);
+
+    } 
 }
